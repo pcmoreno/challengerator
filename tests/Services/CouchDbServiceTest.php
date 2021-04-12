ac4aec7b3b291872542be2f4be22ca414eb43662
Paulo Moreno <pcmoreno@yahoo.com>
2021-04-12 16:28:43 +0200

    disable not finished test

--- tests/Services/CouchDbServiceTest.php
+++ tests/Services/CouchDbServiceTest.php
@@ -6,7 +6,6 @@ namespace App\Tests\Services;
 use App\Services\CouchDbService;
 use \Mockery as m;
 use Mockery\Adapter\Phpunit\MockeryPHPUnitIntegration;
-use phpDocumentor\Reflection\Types\This;
 use PHPOnCouch\CouchClient;
 use PHPUnit\Framework\TestCase;
 use Symfony\Component\HttpFoundation\JsonResponse;
@@ -27,15 +26,15 @@ class CouchDbServiceTest extends TestCase
         $this->service = m::mock(new CouchDbService('testDsn', 'testdb'));
     }
 
-    public function test_it_should_try_to_create_document_with_the_correct_data()
-    {
-        $response = $this->service->createDocument(null, ["a"=>"b"], $this->client);
-
-        $this->client->shouldHaveBeenCalled()
-            ->once()
-            ->withAnyArgs()
-        ;
-
-        $this->assertEquals(200, $response->getStatusCode());
-    }
+//    public function test_it_should_try_to_create_document_with_the_correct_data()
+//    {
+//        $response = $this->service->createDocument(null, ["a"=>"b"], $this->client);
+//
+//        $this->client->shouldHaveBeenCalled()
+//            ->once()
+//            ->withAnyArgs()
+//        ;
+//
+//        $this->assertEquals(200, $response->getStatusCode());
+//    }
 }
