ff6363cabe65e627d1b747bdeda69492f70fa834
Paulo Moreno <pcmoreno@yahoo.com>
2021-09-07 16:15:19 +0200

    allow file uploads to googleDrive

---
+++ src/Services/GoogleDriveService.php
@@ -0,0 +1,103 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Services;
+
+use Exception;
+use Google_Client;
+use Google_Service_Drive;
+use Google_Service_Drive_DriveFile;
+
+class GoogleDriveService
+{
+    private Google_Client $googleClient;
+    private Google_Service_Drive $googleServiceDrive;
+
+    public function __construct()
+    {
+        $this->googleClient = $this->getClient();
+        $this->googleServiceDrive = new Google_Service_Drive($this->googleClient);
+    }
+
+    public function listFilesInFolder(string $folderId)
+    {
+        $optParams = array(
+            'pageSize' => 100,
+            'fields' => "nextPageToken, files(contentHints/thumbnail,fileExtension,iconLink,id,name,size,thumbnailLink,webContentLink,webViewLink,mimeType,parents)",
+            'q' => "'".$folderId."' in parents"
+        );
+        $results = $this->googleServiceDrive->files->listFiles($optParams);
+
+        return ($results->getFiles());
+    }
+
+    public function uploadFileToGoogleDrive($driveFile, $folderId): string
+    {
+        $fileMetadata = new Google_Service_Drive_DriveFile(['name' => $driveFile['name']]);
+        $fileMetadata->setParents([$folderId]);
+        $content = file_get_contents($driveFile['tmp_name']);
+        $mimeType = mime_content_type($driveFile['tmp_name']);
+
+        try {
+            $file = $this->googleServiceDrive->files->create(
+                $fileMetadata, [
+                    'data' => $content,
+                    'mimeType' => $mimeType,
+                    'fields' => 'id'
+                ]
+            );
+        } catch (Exception $exception) {
+            return 'failed';
+        }
+        return $file->id;
+    }
+
+    private function getClient(): Google_Client
+    {
+        $client = new Google_Client();
+        $client->setApplicationName('Google Drive API PHP Quickstart');
+        $client->setScopes(Google_Service_Drive::DRIVE);
+        $client->setAuthConfig('../credentials.json');
+        $client->setAccessType('offline');
+        $client->setPrompt('select_account consent');
+
+        // Load previously authorized token from a file, if it exists.
+        // The file token.json stores the user's access and refresh tokens, and is
+        // created automatically when the authorization flow completes for the first
+        // time.
+        $tokenPath = '../token.json';
+        if (file_exists($tokenPath)) {
+            $accessToken = json_decode(file_get_contents($tokenPath), true);
+            $client->setAccessToken($accessToken);
+        }
+
+        // If there is no previous token or it's expired.
+        if ($client->isAccessTokenExpired()) {
+            // Refresh the token if possible, else fetch a new one.
+            if ($client->getRefreshToken()) {
+                $client->fetchAccessTokenWithRefreshToken($client->getRefreshToken());
+            } else {
+                // Request authorization from the user.
+                $authUrl = $client->createAuthUrl();
+                printf("Open the following link in your browser:\n%s\n", $authUrl);
+                print 'Enter verification code: ';
+                $authCode = trim(fgets(STDIN));
+
+                // Exchange authorization code for an access token.
+                $accessToken = $client->fetchAccessTokenWithAuthCode($authCode);
+                $client->setAccessToken($accessToken);
+
+                // Check to see if there was an error.
+                if (array_key_exists('error', $accessToken)) {
+                    throw new Exception(join(', ', $accessToken));
+                }
+            }
+            // Save the token to a file.
+            if (!file_exists(dirname($tokenPath))) {
+                mkdir(dirname($tokenPath), 0700, true);
+            }
+            file_put_contents($tokenPath, json_encode($client->getAccessToken()));
+        }
+        return $client;
+    }
+}
